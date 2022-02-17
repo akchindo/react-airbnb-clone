d862498832b13daeb7d2aba90e154a1aa655bbad
akchindo <abdullahichindo16@gmail.com>
2022-02-17 13:46:58 +0100

    implementing the props

--- src/App.js
+++ src/App.js
@@ -3,39 +3,60 @@ import Navbar from "./components/NavBar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Card from "./components/Card/Card";
 import personImage from "../src/components/Images/Person.png"
+import wedding from "../src/components/Images/wedding-photography 1.png"
+import bike from "../src/components/Images/mountain-bike 1.png"
+import data from "./data";
+// import personImagea from '../public/Images/Person.png'
+
+ const cards = data.map((item) => {
+   return < Card 
+      imageP={item.coverImg} 
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      country={item.location}
+      title={item.title}
+      price={item.price}      
+  />
+ })
 
 
 function App () {
+
+  console.log('checking data', data[0].coverImg)
     return (
       <div>
         <Navbar />
         <Hero />
-        <Card 
-        imageP={personImage}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title= "Life lessons with Katie Zaferes"
-        price= {136}
-        />
-         <Card 
-        imageP={personImage}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title= "Life lessons with Katie Zaferes"
-        price= {136}
-        />
-         <Card 
-        imageP={personImage}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title= "Life lessons with Katie Zaferes"
-        price= {136}
-        />
+        {cards}
+        
+
+
       </div>
     )
 }
+
+// const num = [1, 2, 3, 4, 5]
+
+// const square = num.map((n)=> {
+//  return n**2
+ 
+// })
+// console.log(square)
+
+// const names = ["liman", "tycoon", "doings"]
+
+// const upperCase = names.map((name) => {
+//   return name[0].toUpperCase() + name.slice(1)
+
+// })
+
+// console.log(upperCase)
+
+// const html = names.map((name) => {
+//   return `<p>${name}</p>`
+
+// })
+
+// console.log(html)
 
 export default App;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,19 +5,22 @@ import star from "../Images/Star.png"
 
 function Card(props) {
     console.log(props)
-    
+
   return (
-    <div className='card'>
-        <img src={props.imageP} alt="" className='cardImage'/>
-        <div className='classStats'>
-            <img src={star} alt=""  className='cardStar'/>
-            <span>{props.rating}</span>
-            <span className='gray'>{props.reviewCount} .</span>
-            <span className='gray'>USA</span>
+      <div className="cardList">
+        <div className='card'>
+            <img src={`/images/${props.imageP}`} alt="" className='cardImage'/>
+            <div className='classStats'>
+                <img src={star} alt=""  className='cardStar'/>
+                <span>{props.rating}</span>
+                <span className='gray'>({props.reviewCount}) .</span>
+                <span className='gray'>USA</span>
+            </div>
+                <p>{props.title}</p>
+                <p> <span className='bold'> From ${props.price} </span> / person</p>
         </div>
-            <p>{props.title}</p>
-            <p> <span className='bold'> From $136 </span> / person</p>
-    </div>
+     </div>
+
     )
 }
 
