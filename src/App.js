7bbf481f47d06d0dee79b6aba1c847529e6bf983
akchindo <abdullahichindo16@gmail.com>
2022-02-18 15:15:50 +0100

    passing the props dynamically

--- src/App.js
+++ src/App.js
@@ -2,44 +2,48 @@ import React from "react";
 import Navbar from "./components/NavBar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Card from "./components/Card/Card";
-import personImage from "../src/components/Images/Person.png"
-import wedding from "../src/components/Images/wedding-photography 1.png"
-import bike from "../src/components/Images/mountain-bike 1.png"
+import personImage from "../src/components/Images/Person.png";
+import wedding from "../src/components/Images/wedding-photography 1.png";
+import bike from "../src/components/Images/mountain-bike 1.png";
 import data from "./data";
+import "./styles/styles.css";
 // import personImagea from '../public/Images/Person.png'
 
- const cards = data.map((item) => {
-   return < Card 
-      imageP={item.coverImg} 
-      rating={item.stats.rating}
-      reviewCount={item.stats.reviewCount}
-      country={item.location}
-      title={item.title}
-      price={item.price}      
-  />
- })
+const cards = data.map((item) => {
+  //  return < Card
+  //     key={item.id}
+  //     imageP={item.coverImg}
+  //     rating={item.stats.rating}
+  //     reviewCount={item.stats.reviewCount}
+  //     country={item.location}
+  //     title={item.title}
+  //     price={item.price}
+  //     openSpot={item.openSpots}
+  // />
 
+  return <Card 
+          key={item.id} 
+          {...item}
+          // item={item}
+          />;
+});
 
-function App () {
-
-  console.log('checking data', data[0].coverImg)
-    return (
-      <div>
-        <Navbar />
-        <Hero />
-        {cards}
-        
-
-
-      </div>
-    )
+function App() {
+  console.log("checking data", data[0].coverImg);
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <section className='cardList'>{cards}</section>
+    </div>
+  );
 }
 
 // const num = [1, 2, 3, 4, 5]
 
 // const square = num.map((n)=> {
 //  return n**2
- 
+
 // })
 // console.log(square)
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,21 +5,28 @@ import star from "../Images/Star.png"
 
 function Card(props) {
     console.log(props)
+    let badgeText 
+
+    if(props.openSpots ===0) {
+        badgeText = 'SOLD OUT'
+    } else if (props.location === 'Online') {
+        badgeText = 'ONLINE'
+    }
 
   return (
-      <div className="cardList">
         <div className='card'>
-            <img src={`/images/${props.imageP}`} alt="" className='cardImage'/>
+            {/* { props.openSpot === 0 && <div className='cardBadge'>SOLD OUT</div>} */}
+           {badgeText && <div className='cardBadge'>{badgeText}</div>}
+            <img src={`/images/${props.coverImg}`} alt="" className='cardImage'/>
             <div className='classStats'>
                 <img src={star} alt=""  className='cardStar'/>
-                <span>{props.rating}</span>
-                <span className='gray'>({props.reviewCount}) .</span>
+                <span>{props.stats.rating}</span>
+                <span className='gray'>({props.stats.rating}) .</span>
                 <span className='gray'>USA</span>
             </div>
-                <p>{props.title}</p>
+                <p  className='cardTitle'>{props.title}</p>
                 <p> <span className='bold'> From ${props.price} </span> / person</p>
         </div>
-     </div>
 
     )
 }
